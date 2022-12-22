27ad7af8a85f80519b2b8d2e45226be2fe52c41f
Nathan Maria <nathanms2000@outlook.com>
2022-12-22 00:43:03 -0300

    Subindo frontend

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,36 @@
+import { useContext } from 'react'
+import styles from './styles.module.scss'
+import Link from 'next/link'
+import {FiLogOut} from 'react-icons/fi'
+
+import {AuthContext} from '../../contexts/AuthContext'
+
+export function Header(){
+    const {signOut} = useContext(AuthContext)
+
+
+    return(
+        <header className={styles.headerContainer}>
+            <div className={styles.headerContent}>
+                <Link href="/dashboard">
+                    <img src='/logo.png' width={170} height={140} />
+                </Link>
+
+                <nav className={styles.menuNav}>
+                    <Link href="/category" legacyBehavior>
+                        <a>Categoria</a>
+                    </Link>
+
+                    <Link href="/product" legacyBehavior>
+                        <a>Cardapio</a>
+                    </Link>
+
+                    <button onClick={signOut}>
+                        <FiLogOut color="#FFF" size={24}/>
+                    </button>
+
+                </nav>
+            </div>
+        </header>
+    )
+}
